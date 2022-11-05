a6f959c97bf5fdb7fc888f73ebae1d6f3e824657
urinovfarrukh <farruxurinov00@gmail.com>
2022-11-05 18:41:04 +0500

    edit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+
+let agree = prompt("Biz uyda dam olamiz (Ehtimoliy javoblar dam olish kuni yoki bayramlarda )");
+
+if (agree === "dam olish kuni" || agree === "Y"){
+    console.log("true");
+}else if (agree === "bayramlarda"){
+    console.log("false");
+}
